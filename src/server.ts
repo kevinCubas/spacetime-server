a29afcedfa2c998ca776c2cdc586ba9ba80a47cb
unknown <61201131+kevinCubas@users.noreply.github.com>
2023-05-17 15:18:12 -0300

    feat: prisma model

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,14 @@
+import { PrismaClient } from '@prisma/client'
 import fastify from 'fastify'
 
 const app = fastify()
+const prisma = new PrismaClient()
+
+app.get('/', async () => {
+  const users = await prisma.user.findMany()
+
+  return { users }
+})
 
 app
   .listen({
