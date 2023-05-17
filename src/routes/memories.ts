3c246bea9d38cb5b025cd22e976dbc212c5bdf6c
unknown <61201131+kevinCubas@users.noreply.github.com>
2023-05-17 16:51:42 -0300

    feat: get all memories

--- src/routes/memories.ts
+++ src/routes/memories.ts
@@ -1,7 +1,22 @@
 import { FastifyInstance } from 'fastify'
+import { prisma } from '../lib/prisma'
 
 export async function memoriesRoutes(app: FastifyInstance) {
-  app.get('/memories', async () => {})
+  app.get('/memories', async () => {
+    const memories = await prisma.memory.findMany({
+      orderBy: {
+        createdAt: 'asc',
+      },
+    })
+
+    return memories.map((memory) => {
+      return {
+        id: memory.id,
+        coverUrl: memory.coverUrl,
+        excerpt: memory.content.substring(0, 115).concat('...'),
+      }
+    })
+  })
 
   app.get('/memories/:id', async () => {})
 
