97b9f9deed69b3376f83f3ba2f6be31039eb86e1
unknown <61201131+kevinCubas@users.noreply.github.com>
2023-05-17 16:59:01 -0300

    feat: get memory by id

--- src/routes/memories.ts
+++ src/routes/memories.ts
@@ -1,5 +1,6 @@
 import { FastifyInstance } from 'fastify'
 import { prisma } from '../lib/prisma'
+import { z } from 'zod'
 
 export async function memoriesRoutes(app: FastifyInstance) {
   app.get('/memories', async () => {
@@ -18,7 +19,21 @@ export async function memoriesRoutes(app: FastifyInstance) {
     })
   })
 
-  app.get('/memories/:id', async () => {})
+  app.get('/memories/:id', async (request) => {
+    const paramsSchema = z.object({
+      id: z.string().uuid(),
+    })
+
+    const { id } = paramsSchema.parse(request.params)
+
+    const memory = await prisma.memory.findUniqueOrThrow({
+      where: {
+        id,
+      },
+    })
+
+    return memory
+  })
 
   app.post('/memories/', async () => {})
 
