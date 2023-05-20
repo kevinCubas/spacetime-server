1778589c1baa60f08e2815e8550d542bdef53dbd
unknown <61201131+kevinCubas@users.noreply.github.com>
2023-05-20 19:05:11 -0300

    feat: upload route

---
+++ src/routes/upload.ts
@@ -0,0 +1,50 @@
+import { randomUUID } from 'node:crypto'
+import { FastifyInstance } from 'fastify'
+import { extname, resolve } from 'node:path'
+import { createWriteStream } from 'node:fs'
+import { promisify } from 'node:util'
+import { pipeline } from 'node:stream'
+
+const pump = promisify(pipeline)
+
+export async function uploadRoutes(app: FastifyInstance) {
+  app.post('/upload', async (request, reply) => {
+    const upload = await request.file({
+      limits: {
+        fileSize: 5_242_880, // 5mb
+      },
+    })
+
+    if (!upload) {
+      return reply.status(400).send({
+        message: 'No file uploaded',
+      })
+    }
+
+    const mimeTypeRegex = /^(video|image)\/[a-zA-Z/]+/
+    const isValidFileFormat = mimeTypeRegex.test(upload.mimetype)
+
+    if (!isValidFileFormat) {
+      return reply.status(400).send({
+        message: 'Invalid file format',
+      })
+    }
+
+    const fileId = randomUUID() // generate random uuid
+    const extension = extname(upload.filename) // get the file actual name
+
+    const fileName = fileId.concat(extension) // change the file name concatenated with uuid
+
+    const writeStream = createWriteStream(
+      resolve(__dirname, '../../uploads', fileName), // stream the file
+    )
+
+    await pump(upload.file, writeStream)
+
+    const fullUrl = request.protocol.concat('://').concat(request.hostname)
+    const fileUrl = new URL(`uploads/${fileName}`, fullUrl).toString()
+
+    console.log(fileUrl)
+    return { ok: true }
+  })
+}
--- src/server.ts
+++ src/server.ts
@@ -3,11 +3,21 @@ import 'dotenv/config'
 import fastify from 'fastify'
 import cors from '@fastify/cors'
 import jwt from '@fastify/jwt'
+import multipart from '@fastify/multipart'
+import staticFastify from '@fastify/static'
 import { memoriesRoutes } from './routes/memories'
-import { auth } from './routes/auth'
+import { authRoutes } from './routes/auth'
+import { uploadRoutes } from './routes/upload'
+import { resolve } from 'path'
 
 const app = fastify()
 
+app.register(multipart)
+
+app.register(staticFastify, {
+  root: resolve(__dirname, '../uploads'),
+  prefix: '/uploads',
+})
 app.register(cors, {
   origin: true,
 })
@@ -15,8 +25,9 @@ app.register(jwt, {
   secret: 'roeofdposdfp-_0+[2$%j/*+.redgr65476rtrg',
 })
 
+app.register(authRoutes)
+app.register(uploadRoutes)
 app.register(memoriesRoutes)
-app.register(auth)
 
 app
   .listen({
